cfb7ed643d65acca4f7c2a88460adc9be2fd2f86
Tatiane Moraes <tatimoraesdev@gmail.com>
2021-09-14 17:40:59 -0300

    Create Room

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,9 +1,11 @@
-import { Link } from 'react-router-dom';
+import { FormEvent, useState } from 'react'; 
+import { Link, useHistory } from 'react-router-dom';
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg';
 
 import { Button } from '../components/Button';
+import { database } from '../services/firebase';
 import { useAuth } from '../hooks/useAuth';
 
 import '../styles/auth.scss';
@@ -11,7 +13,25 @@ import '../styles/auth.scss';
 //webpack (module bundler)
 
 export function NewRoom () {
-    //const { user } = useAuth();
+    const { user } = useAuth();
+    const history =useHistory()
+    const [newRoom, setNewRoom] = useState('');
+
+    async function handleCreateRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if (newRoom.trim() === ''){
+
+        }
+
+        const roomRef = database.ref('rooms');
+        const firebaseRoom = await roomRef.push({
+            title: newRoom,
+            authorId: user?.id,
+        })
+
+        history.push(`/rooms/${firebaseRoom.key}`)
+    }
 
     return (
         <div id="page-auth">
@@ -25,10 +45,12 @@ export function NewRoom () {
                     <img src={logoImg} alt="Letmeask" />
                     <h2>Criar uma nova sala</h2>
                     <div className="separator">ou entre em uma sala</div>
-                    <form>
+                    <form onSubmit={handleCreateRoom}>
                         <input 
-                        type="text"
-                        placeholder="Nome da sala" 
+                            type="text"
+                            placeholder="Nome da sala" 
+                            onChange={event => setNewRoom(event.target.value)}
+                            value={newRoom}
                         />
                         <Button type="submit">
                             Criar sala
